500338d030a5d1001514019f6ee1bf46350aae4c
Lee bosung <dlqh406@gmail.com>
2020-08-08 22:45:54 +0900

    “start”

---
+++ public/assets/js/init.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyATIBNzGDyI67V4gxIjz-e2dpnnKRaM4Cw",
+    authDomain: "template2-67115.firebaseapp.com",
+    databaseURL: "https://template2-67115.firebaseio.com",
+    projectId: "template2-67115",
+    storageBucket: "template2-67115.appspot.com",
+    messagingSenderId: "745569096392",
+    appId: "1:745569096392:web:0649ac168e9963338e4bfd",
+    measurementId: "G-HV2F5DEZL6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
